2146fb42fe767739dc52d8ed8966770314a98880
Y3rbit4 <hrodriguez@frba.utn.edu.ar>
2021-04-04 13:54:55 -0300

    Database setting, connection string and refactoring of db-js

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,23 @@ const util = require("util");
 
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 
-let pool = mysql.createConnection(
-  "mysql://bdd4315c716ca2:6263d33d@us-cdbr-east-03.cleardb.com/heroku_4f9f340f759415e?reconnect=true"
-);
+let pool = mysql.createPool({
+  host: DB_HOST || "freedb.tech",
+  port: DB_PORT || 3306,
+  password: DB_PASS || "000cul12",
+  user: DB_USER || "freedbtech_yerbita",
+  database: DB_NAME || "freedbtech_tedxutn",
+  connectionLimit: 10,
+});
 
 pool.query = util.promisify(pool.query);
+
+pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+  if (error) {
+    console.log(`error al conectar con la db ${error}`);
+  } else {
+    console.log("Se conecto a la db ");
+  }
+});
 
 module.exports = pool;

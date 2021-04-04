765a798e88174b52a2d64bf2d9ee9cb943ce31f2
Y3rbit4 <hrodriguez@frba.utn.edu.ar>
2021-04-04 02:31:47 -0300

    Database setting, connection string and refactoring of db-js

--- utils/db.js
+++ utils/db.js
@@ -3,14 +3,9 @@ const util = require("util");
 
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env;
 
-let pool = mysql.createPool({
-  host: DB_HOST || "localhost",
-  port: DB_PORT || 3306,
-  password: DB_PASS || "",
-  user: DB_USER || "root",
-  database: DB_NAME || "tedxutn",
-  connectionLimit: 10,
-});
+let pool = mysql.createConnection(
+  "mysql://bdd4315c716ca2:6263d33d@us-cdbr-east-03.cleardb.com/heroku_4f9f340f759415e?reconnect=true"
+);
 
 pool.query = util.promisify(pool.query);
 
